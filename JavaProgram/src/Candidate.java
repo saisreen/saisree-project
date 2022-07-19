77d4061d0eab4c4b219b9db7ba6a8395058f8d45
sai sri <saisreenss@gmail.com>
2022-07-19 12:10:33 -0500

    Implementing Method Overlaoding

--- JavaProgram/src/Candidate.java
+++ JavaProgram/src/Candidate.java
@@ -1,9 +1,30 @@
 
 class Examiner {
 	
-	void hallticket() {
-	     System.out.println("Verify the Candidate hallticket as 1.Name 2.Hallticket number 3.Course_details ");
+	
+	
+	//void hallticket() {
+	//System.out.println("Examiner will Check the Candidate 1.Name 2.Hallticket number 3.Course_details ");
+//}
+	
+	//Method Overloading
+	
+	void hallticket(String verify1 ) {
+		System.out.println("Examiner will Check the Candidate "+verify1);
 	}
+	 
+	//Method Overloading
+	
+	void hallticket(String verify1, String verify2 ) {
+		System.out.println("Examiner will Check the Candidate "+verify1+" and "+verify2);
+	}
+	
+	//Method Overloading
+	
+	void hallticket(String verify1, String verify2, String verify3 ) {
+		System.out.println("Examiner will Check the Candidate "+verify1+ "," +verify2+ " and " +verify3);
+	}
+	
 	
 	void takeanexampaper(String Course) {
 		System.out.println("Write an exam in " +Course);
@@ -28,8 +49,19 @@ public class Candidate {
 
 		//Creating an Object for an Examiner
 		Examiner durga = new Examiner();
+		    
 		
-		durga.hallticket();                     //Method call
+	   
+		// Examiner check only the Hallticket number 
+		durga.hallticket(" 1.Hallticket number ");  
+		
+		//Examiner check only the two details to verify
+		durga.hallticket(" 1.Hallicket number ", " 2.image Verification");
+		
+		//Examiner check all three details to verify
+		durga.hallticket(" 1.Hallticket number ", " 2.Name ", " 3.image Verification");   
+		
+		
 		durga.takeanexampaper("Maths");
 		System.out.println("The exam duration is " +durga.examduration());
 		System.out.println("you are " +durga.results(98));
--- JavaProgram/src/Customer.java
+++ JavaProgram/src/Customer.java
@@ -1,13 +1,43 @@
 
 class Bank {
 	
-	void form () {
-		System.out.println("Fill the Form 1.Name 2.Age 3.Address 4. Contact details 5. Guardian details");
+	
+	//Method Overloading
+	
+	void form (String form1, String form2) {
+		System.out.println("Collect the Application form "+form1+" or "+form2);
 	}
 	
-	void proof (String id, String address) {
-		System.out.println("Proof submitted are " +id+" and " +address);
+	
+	void form () {
+			System.out.println("Fill the Form 1.Name 2.Age 3.Address 4. Contact details 5. Guardian details");
+		}
+	
+	
+	//void proof (String id, String address) {
+		//System.out.println("Proof submitted are " +id+" and " +address);
+	//}
+	
+	
+	//Method Overloading 
+	
+		void proof (String id1, String id2) {
+			System.out.println("Submit Proof  " +id1+" and "+id2);
+		}
+	
+
+	//Method Overloading 
+	
+	void proof (String id1, String id2, String id3) {
+		System.out.println("submit proof " +id1+","+id2+","+" and "+id3);
 	}
+	
+	//Method Overloading 
+	
+	void proof (String id1, String id2, String id3, String id4) {
+		System.out.println("submit proof " +id1+","+id2+","+id3+" and "+id4);
+	}
+	
 	
 	String passbook () {
 		return "201923467";
@@ -27,12 +57,24 @@ public class Customer {
 		// TODO Auto-generated method stub
 		
 		//Creating an Object for bank class
-		Bank sai = new Bank();
+		Bank Comerca = new Bank();
 		
-		sai.form();                                                                             //Method call
-		sai.proof("passport", "current_bill");                                                  
-		System.out.println("Bank gave passbook and account number is " + sai.passbook());            
-		System.out.println("Bank gave " + sai.deposit(2000));                                        
+	    Comerca.form("1.Savings Acoount ", "2.Current account ");     
+		Comerca.form();
+		
+		//submit only two proofs 
+		Comerca.proof(" 1.Passport "," 2. Driver's License ");
+		
+		//submit all of these three proofs
+		Comerca.proof(" 1.Passport ", " 2.Driver's License ", " 3.SSN ");
+		
+		//submit all of these four proofs 
+		Comerca.proof(" 1.Passport ", " 2.Driver's License ", " 3.SSN ", " 4.Utility Bill ");
+		
+		
+		                                                 
+		System.out.println("Bank gave passbook and account number is " + Comerca.passbook());            
+		System.out.println("Bank gave " + Comerca.deposit(2000));                                        
 		
 		
 
