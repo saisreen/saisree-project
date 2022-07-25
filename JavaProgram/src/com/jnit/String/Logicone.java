203b6cf8c75d6636d79386f563c89a5a357fd4c1
sai sri <saisreenss@gmail.com>
2022-07-25 11:13:51 -0500

    Assignment -7

---
+++ JavaProgram/src/com/jnit/String/Logicone.java
@@ -0,0 +1,18 @@
+package com.jnit.String;
+
+public class Logicone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int  n=5, i=1;
+		System.out.println("Five Times Table");
+		for(i=1; i<=10; i++) {
+			//Table =n*1;
+			System.out.println(n+" x "+i+ " = "+n*i);
+		}
+		
+		
+	}
+}
+
---
+++ JavaProgram/src/com/jnit/String/Logictwo.java
@@ -0,0 +1,18 @@
+package com.jnit.String;
+
+public class Logictwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int n=5,i=2, sum = 1;
+		while(i<=n) {
+			sum = sum + i;
+			i++;
+			System.out.println("sum = "+sum);
+		}
+		
+	}
+
+}
+
