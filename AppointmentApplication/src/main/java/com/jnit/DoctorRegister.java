33bbe3487246d4f11be458e6a80459ef600991be
sai sri <saisreenss@gmail.com>
2022-08-28 16:09:23 -0500

    Assignment-17

---
+++ AppointmentApplication/src/main/java/com/jnit/DoctorRegister.java
@@ -0,0 +1,71 @@
+package com.jnit;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DoctorRegister extends HttpServlet {
+	
+	Connection connection = null;
+	PreparedStatement ps = null;
+	
+	public void init(ServletConfig config) {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		String url ="jdbc:mysql://localhost:3306/jnit";
+		String username = "root";
+		String password ="root";
+		try {
+			connection  = DriverManager.getConnection(url, username, password);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+      public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException  {
+    	  String name = request.getParameter("name");
+    	  String password = request.getParameter("password");
+    	  String email = request.getParameter("email");
+    	  String specialisation = request.getParameter("specialisation");
+    	  String mobile = request.getParameter("xyz");
+    	  long phone=Long.parseLong(mobile);
+    	  PrintWriter pw=response.getWriter();
+    	  String sql="insert into doctor(name, password, email, phone, specialisation) values(?,?,?,?,?)";
+    	  try {
+    	  ps=connection.prepareStatement(sql);
+    	  ps.setString(1,name);
+    	  ps.setString(2, password);
+    	  ps.setString(3, email);
+    	  ps.setLong(4, phone);
+    	  ps.setString(5, specialisation);
+    	  int x=ps.executeUpdate();
+    	  pw.println("<html><body bgcolor='wheat'><h1 align='center'>");
+    	  if(x!=0)
+    		  pw.println("Doctor Registered Successfully");
+    	  pw.println("</h1></body></html>");
+    	  
+      } catch (SQLException e) {
+    	  e.printStackTrace();
+}
+      }
+      public void destroy() {
+    	try {
+			connection.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+}
+}
