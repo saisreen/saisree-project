f91818ff944a176f98a904749ef9e713e81cee4a
sai sri <saisreenss@gmail.com>
2023-06-15 11:31:34 -0500

    Type Casting & Conversion

---
+++ JavaProgram/src/TypeConversionTypeCasting/TypeCasting.java
@@ -0,0 +1,17 @@
+package TypeConversionTypeCasting;
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double d = 20.8;
+		
+		int i;
+		i = (int) d; //Type casting - Narrowing Type casting double to int 
+		
+		System.out.println(i);
+
+	}
+
+}
---
+++ JavaProgram/src/TypeConversionTypeCasting/TypeConversion.java
@@ -0,0 +1,21 @@
+package TypeConversionTypeCasting;
+
+public class TypeConversion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	int i = 12;
+	
+	double d = i;   //widnening type conversion int to double 
+	
+	System.out.println(d);
+	
+		
+	
+		
+		
+
+	}
+
+}
