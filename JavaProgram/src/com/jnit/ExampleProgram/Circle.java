e01306d48bdce7ee015fcc826acccc150bc124b1
sai sri <saisreenss@gmail.com>
2023-06-15 11:31:21 -0500

    Examples of Java Program

---
+++ JavaProgram/src/com/jnit/ExampleProgram/Circle.java
@@ -0,0 +1,18 @@
+package com.jnit.ExampleProgram;
+
+
+public class Circle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double area;
+		int r = 1;
+		
+		area = 3.1414 * r * r;
+		
+		System.out.println(area);
+
+	}
+
+}
---
+++ JavaProgram/src/com/jnit/ExampleProgram/SimpleInterest.java
@@ -0,0 +1,17 @@
+package com.jnit.ExampleProgram;
+
+public class SimpleInterest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double si;
+		int Principal = 100000;
+		int time = 2;
+		float rate = 2.5f;
+		si= Principal * time * rate /100;
+		System.out.println(si);
+
+	}
+
+}
